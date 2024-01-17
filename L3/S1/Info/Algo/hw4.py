569f1cb25778640863e883e67d7486840bd80b6d
Pandada42 <mb692906@gmail.com>
2024-01-17 23:07:52 +0100

    fin homework 4

--- L3/S1/Info/Algo/hw4.py
+++ L3/S1/Info/Algo/hw4.py
@@ -13,20 +13,36 @@ def dijkstra(s, t, g):
         M.add(u)
         for v in range(len(g)):
             w = g[u][v]
+            if w == 0:
+                continue
 
             if v in M:
                 continue
             dv = du + w
-            if y not in d or d[y] > dy : 
-                heappush(suivants, (dy, y))
-                p[y] = u
-            elif d[y] == dy : 
-                p[y].append(u)
-                heappush(suivants, (dy, y))
-    paths = [t]
-    x = t
-    while x != s: 
-        x = tuple(p[x])
-        paths.insert(0, x)
+            if v not in d or d[v] > dv : 
+                heappush(suivants, (dv, v))
+                p[v] = [u]
+                d[v] = dv
+            elif d[v] == dv : 
+                p[v].append(u)
+                heappush(suivants, (dv, v))
+
+    paths = [set([t])]
+    while True:
+        rpo = set()
+        for x in paths[0]:
+            rpo = rpo.union(set(p[x]))
+        paths.insert(0, rpo)
+        if s in paths[0]:
+            break
     return paths
 
+def short_edge(g, s, t, u, v):
+    p = dijkstra(s, t, g)
+    if not p :
+        return True
+    for i in range(len(p)):
+        if u in p[i] :
+            if v in p[i+1]:
+                return True 
+    return False
